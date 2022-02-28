3416585e53c353f0088037f6b8561621734ef4fa
Mahmoud THIAM <dooro.mah@gmail.com>
2022-02-28 15:38:34 +0000

    Mis à niveau de la NavBar

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
 import './normalize.css';
-import './style.css';
+import './css/style.css'
+const ul = document.querySelector('#navigation ul')
+ul.addEventListener("mouseover", e=>{
+    e.preventDefault;
+    ul.style.top =  "9%"
+    ul.style.transitionProperty = "height"
+    ul.style.transitionDelay= "2s"
+    ul.style.transitionDuration= "1s"
+    ul.style.transitionTimingFunction= "cubic-bezier(0.215, 0.610, 0.355, 1)"
+})
+
+ul.addEventListener("mouseleave", e=> {
+    e.preventDefault;
+    ul.style.top= "1%"
+})
